1366fe77e91509db2d74e2f4ae09e43d9907b526
Nathaly Barbieri <ndelimabarbieri@gmail.com>
2025-06-15 18:14:04 -0300

    first commit

---
+++ Sorteio de número/app/src/main/java/com/example/appsorteioemkotlyn/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.appsorteioemkotlyn
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+
+    fun sorteio(view: View){
+
+        val textResultado  = findViewById<TextView>(R.id.text_resultado)
+        val numero = Random.nextInt(until = 11)
+
+        textResultado.setText("Número Sorteado: $numero")
+    }
+}
