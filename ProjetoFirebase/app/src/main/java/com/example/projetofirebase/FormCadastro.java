21d1f6fcde03dfd6f18bc4576b329ceb548bc448
Nathaly Barbieri <ndelimabarbieri@gmail.com>
2025-06-16 00:52:52 -0300

    ADICIONANDO A PRIMEIRÁ PARTE DO PROJETO DE AUTENTICAÇÃO DE USUÁRIO USANDO FIREBASE

---
+++ ProjetoFirebase/app/src/main/java/com/example/projetofirebase/FormCadastro.java
@@ -0,0 +1,35 @@
+package com.example.projetofirebase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.text.Normalizer;
+
+public class FormCadastro extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_form_cadastro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+
+        });
+    }
+
+
+}
